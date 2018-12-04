85da62fd1fb847e91348618187747672afeea2d2
Eugene Bokhan <eugenebokhan@protonmail.com>
2018-12-04 17:29:12 +0300

    allowedFileExtensions

--- AssetImportKit/AssetImportKit/Extensions/SceneKit/SCNScene/SCNScene + AssetImport.swift
+++ AssetImportKit/AssetImportKit/Extensions/SceneKit/SCNScene/SCNScene + AssetImport.swift
@@ -19,57 +19,30 @@ public extension SCNScene {
     
     // MARK: - Loading scenes using assimp
     
-    /**
-     @name Loading scenes using assimp
-     */
-    /**
-     Returns the array of file extensions for all the supported formats.
-     
-     @return The array of supported file extensions
-     */
-    public class func allowedFileExtensions() -> [String]? {
-        
-        if let extsFile = Bundle(for: AssetImporterScene.self).path(forResource: "valid-extensions", ofType: "txt") {
-            do {
-                let extsFileContents = try String(contentsOfFile: extsFile, encoding: .utf8)
-                
-                if let validExts = (extsFileContents.components(separatedBy: CharacterSet.whitespacesAndNewlines) as NSArray?)?.filtered(using: NSPredicate(format: "self != \"\"")) as? [String] {
-                    return validExts
-                } else {
-                    return nil
-                }
-            } catch {
-                print("The file could not be loaded")
-            }
-        }
-        
-        return nil
+    /// Loading scenes using assimp
+    ///
+    /// - Returns: Returns the array of file extensions for all the supported formats.
+    public class func allowedFileExtensions() -> [String] {
+        return ["dae", "fbx", "obj", "scn", "md3", "zgl", "xgl", "wrl", "stl", "smd", "raw", "q3s", "q3o", "ply", "xml", "mesh", "off", "nff", "m3sd", "md5anim", "md5mesh", "md2", "irr", "ifc", "dxf", "cob", "bvh", "b3d", "blend", "hmp", "3ds", "3d",  "ms3d", "mdl", "ase", "gltf"]
     }
     
-    /**
-     Returns a Boolean value that indicates whether the SCNAssimpScene class can
-     read asset data from files with the specified extension.
-     
-     @param extension The filename extension identifying an asset file format.
-     @return YES if the SCNAssimpScene class can read asset data from files with
-     the specified extension; otherwise, NO.
-     */
+    /// Returns a Boolean value that indicates whether the SCNAssimpScene class can
+    /// read asset data from files with the specified extension.
+    ///
+    /// - Parameter extension: The filename extension identifying an asset file format.
+    /// - Returns: YES if the SCNAssimpScene class can read asset data from files with
+    /// the specified extension; otherwise, NO.
     public class func canImportFileExtension(_ extension: String) -> Bool {
-        if let validExts = AssetImporterScene.allowedFileExtensions() {
-            return validExts.contains(`extension`.lowercased())
-        } else {
-            return false
-        }
+        return allowedFileExtensions().contains(`extension`.lowercased())
     }
     
-    /**
-     Loads a scene from a file with the specified name in the app’s main bundle.
-     
-     @param name The name of a scene file in the app bundle’s resources directory.
-     @param postProcessFlags The flags for all possible post processing steps.
-     @param error Scene loading error.
-     @return A new scene object, or nil if no scene could be loaded.
-     */
+    /// Loads a scene from a file with the specified name in the app’s main bundle.
+    ///
+    /// - Parameters:
+    ///   - filePath: The name of a scene file in the app bundle’s resources directory.
+    ///   - postProcessSteps: The flags for all possible post processing steps.
+    /// - Returns: A new scene object, or nil if no scene could be loaded.
+    /// - Throws: Scene loading error.
     public class func assimpScene(filePath: String,
                                   postProcessSteps: PostProcessSteps) throws -> AssetImporterScene {
         do {
@@ -82,13 +55,13 @@ public extension SCNScene {
         }
     }
     
-    /**
-     Loads a scene from the specified NSString URL.
-     
-     @param url The NSString URL to the scene file to load.
-     @param postProcessFlags The flags for all possible post processing steps.
-     @return A new scene object, or nil if no scene could be loaded.
-     */
+    /// Loads a scene from the specified NSString URL.
+    ///
+    /// - Parameters:
+    ///   - url: The NSString URL to the scene file to load.
+    ///   - postProcessSteps: The flags for all possible post processing steps.
+    /// - Returns: A new scene object, or nil if no scene could be loaded.
+    /// - Throws: Scene loading error.
     public class func assimpScene(with url: URL,
                                   postProcessSteps: PostProcessSteps) throws -> AssetImporterScene {
         do {
